b3ecc17166f1c880349568e95a1cf74253726875
David Ongaro <davidongaroh@gmil.com>
2022-06-16 16:07:44 +0300

    commit tree f95b4b30b79a2bbcbef081ead87d37b0d2f3f73b

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // iterate to the middle
+  for(i=0; i<word.length /2 ; i++){
+    const j = word.length -1 -i
+   /* const startChar= word[i]
+    const endChar= word[j]
+    if (startChar!==endChar) return false
+    else return true */
+
+ if (word[i] !== word[j]) return false
+ 
+ else return true
+}
 }
 
 /* 
   Add your pseudocode here
+  make a fxn that returns true if the reversed word is same as input word
+
+  iterate from the begining to the middle 
+  compare the first letter to the corresponding last letter
+
+  if letters dont match return false 
+  
+  else return true
 */
 
 /*
@@ -20,6 +40,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
 }
 
 module.exports = isPalindrome;
